9e7eb9643f4d9c6241d33c9bf93f9c74dd317fec
xingyanhuan <xingyanhuan@yahoo.co.jp>
2018-10-03 07:15:06 +0900

    typescriptでmocha動かしてみた例

---
+++ typescript/src/listup.ts
@@ -0,0 +1,15 @@
+/**
+ * [listup.ts]
+ *   encoding=UTF8
+ */
+
+
+
+export const listupSubDirectoryPath = ( targetDir: string ) => {
+    let filelsit: string[] = ["hello world.txt"];
+
+    return Promise.resolve( filelsit );
+};
+
+
+
---
+++ typescript/test/listup_test.ts
@@ -0,0 +1,26 @@
+/**
+ * [listup_test.ts]
+ *   encoding=UTF8
+ */
+
+import { describe, it } from "mocha";
+import { expect } from "chai";
+import { shouldFulfilled } from "promise-test-helper";
+
+import { listupSubDirectoryPath } from '../src/listup';
+
+
+describe('TEST for listup_csv_csv.js', () => {
+    describe("::listupSubDirectryPath()", () => {
+        it("Csvファイルが格納されたディレクトリ（1つ下のサブを含む）から、Csvファイルのパスを全て取得する", async () => {
+            const TARGET_DIR:string = "./data/in-stub"; 
+
+            const result:string[] = await listupSubDirectoryPath( TARGET_DIR )
+            .catch((err)=>{throw err;});
+
+            expect( result ).to.deep.equal( ["hello world.txt"] );
+            console.log(result);
+        });
+    });
+});
+
